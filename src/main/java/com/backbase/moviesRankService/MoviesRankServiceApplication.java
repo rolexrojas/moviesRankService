38927f539d0e4485048e5c486db65cc9ef407531
Roberto <Robertorojas@outlook.com>
2021-08-05 20:12:02 -0400

    Initial Commit Choosing Version Libraries and starting the project

---
+++ src/main/java/com/backbase/moviesRankService/MoviesRankServiceApplication.java
@@ -0,0 +1,13 @@
+package com.backbase.moviesRankService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoviesRankServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoviesRankServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/backbase/moviesRankService/MoviesRankServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backbase.moviesRankService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesRankServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
